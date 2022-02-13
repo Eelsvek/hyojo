2928f6d80f9217fb585d35ba025134bf90f6aebe
Kevin <kevin@beacons.ai>
2022-02-13 10:39:04 -0800

    add react-router + routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import NameEmail from './components/Registration/NameEmail';
+import NotFound from './components/NotFound';
 
 function App() {
-  return <div className="App">Yolo</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<div>Yolo</div>} />
+      <Route path="/create-account" element={<NameEmail />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
